b8f572c00985def6c7ff4ef7d332cc557a30cc4f
Jim Eliot <Jim Eliot>
2017-07-19 01:17:18 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 //this is the way an event listner is setup//
-document.getElementById("myform").addEventListener("submit", savebookmark);
+document.getElementById("myform").addEventListener("submit", saveBookmark);
 
-function savebookmark(e){
- var siteName = document.getElementById("name").value;
- var siteUrl = document.getElementById("url").value;
-console.log(siteName, siteUrl);
-e.preventDefault();
+function saveBookmark(e) {
+    var Name = document.getElementById("name").value;
+    var Url = document.getElementById("url").value;
+
+    var bookmark = {
+        name: Name,
+        url: Url
+    }
+
+    if (localStorage.getItem("bookmarks") === null) {
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify("bookmarks"));
+    }
+    else {
+        bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify("bookmarks"));
+    }
+console.log('bookmarks');
+    e.preventDefault();
+}
+
+function fetchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    var render=document.getElementById('filed_items');
+    render.innerHTML='bookmarks'; 
+
 }
