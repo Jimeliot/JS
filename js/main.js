342c74d0fac10f906a7034a98ede8c96c72220a2
Jim Eliot <Jim Eliot>
2017-07-19 19:25:01 +0530

    Finished Product

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ function saveBookmark(e) {
     var Name = document.getElementById("name").value;
     var Url = document.getElementById("url").value;
 
+    if(!formValidate(Name,Url))
+        {
+            return false;
+        }
+
     var bookmark = {
         name: Name,
         url: Url
@@ -47,6 +52,21 @@ function fetchBookmarks() {
     var url = bookmarks[i].url;
         bookRender.innerHTML += '<div class="well">'+'<h3>' + name + ' <a class="btn btn-success" target="_blank" href="' + url + '">visit</a>' + ' <button onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger">Del</button>'+'</h3>'+'</div>';
     }
+}
+
+function formValidate(Name,Url){
+
+    if (!Name || !Url){
+        alert('Please Enter All Details')
+        return false;       
+    }
+
+    if (!Url.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)){
+        alert('Please Enter a valid URL');
+        return false;
+    }
+
+    return true;
 }
 
 /*
